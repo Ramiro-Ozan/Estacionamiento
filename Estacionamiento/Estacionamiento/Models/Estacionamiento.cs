6f07bca6bdc65a2c7ad7bae467a6aefae970b964
MarcosQuiroga99 <MarcosQuiroga1999@hotmail.com>
2024-11-09 22:15:02 -0300

    cambios

---
+++ Estacionamiento/Estacionamiento/Models/Estacionamiento.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Estacionamiento.Models
+{
+    public class Estacionamiento
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Direcion {  get; set; }
+        public List<Vehiculo>? Plaza { get; set; }
+        public List<Cliente>? Clientes { get; set; }
+        public List<Ticket>? Tickets { get; set; }
+        public int MaxPlaza { get; set; }
+
+        public List<TipoVehiculo>? tipoVehiculos { get; set; }
+     }
+}
